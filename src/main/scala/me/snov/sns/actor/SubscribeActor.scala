1773b0049f81fc901b0d9e1cd69512dbe48d547f
Jon Anderson <jon.anderson@smartthings.com>
2017-12-11 14:03:00 -0600

    Added support subscription attributes (RawMessageDelivery) and message attributes

--- src/main/scala/me/snov/sns/actor/SubscribeActor.scala
+++ src/main/scala/me/snov/sns/actor/SubscribeActor.scala
@@ -6,6 +6,8 @@ import akka.actor.Status.{Failure, Success}
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import me.snov.sns.actor.DbActor.CmdGetConfiguration
 import me.snov.sns.model._
+import akka.actor.PoisonPill
+import akka.actor.Status
 
 object SubscribeActor {
   def props(dbActor: ActorRef) = Props(classOf[SubscribeActor], dbActor)
@@ -25,6 +27,9 @@ object SubscribeActor {
   case class CmdDeleteTopic(arn: String)
 
   case class CmdListTopics()
+
+  case class CmdSetSubscriptionAttributes(subscriptionArn: String, attributeName: String, attributeValue: String)
+  case class CmdGetSubscriptionAttributes(subscriptionArn: String)
 }
 
 class SubscribeActor(dbActor: ActorRef) extends Actor with ActorLogging {
@@ -32,10 +37,11 @@ class SubscribeActor(dbActor: ActorRef) extends Actor with ActorLogging {
   import me.snov.sns.actor.SubscribeActor._
   
   type TopicArn = String
+  type SubscriptionArn = String
 
   var topics = Map[TopicArn, Topic]()
   var subscriptions = Map[TopicArn, List[Subscription]]()
-  var actorPool = Map[Subscription, ActorRef]()
+  var actorPool = Map[SubscriptionArn, ActorRef]()
 
   dbActor ! CmdGetConfiguration
 
@@ -43,9 +49,9 @@ class SubscribeActor(dbActor: ActorRef) extends Actor with ActorLogging {
     try {
       if (topics.isDefinedAt(topicArn) && subscriptions.isDefinedAt(topicArn)) {
         subscriptions(topicArn).foreach((s: Subscription) => {
-          if (actorPool.isDefinedAt(s)) {
+          if (actorPool.isDefinedAt(s.arn)) {
             log.debug(s"Sending message ${message.uuid} to ${s.endpoint}")
-            actorPool(s) ! message
+             actorPool(s.arn) ! message
           } else {
             throw new RuntimeException(s"No actor for subscription ${s.endpoint}")
           }
@@ -57,6 +63,45 @@ class SubscribeActor(dbActor: ActorRef) extends Actor with ActorLogging {
       Success
     } catch {
       case e: Throwable => Failure(e)
+    }
+  }
+
+  def findSubscription(subscriptionArn: String): Option[Subscription] = {
+    subscriptions.values.flatten.find{_.arn == subscriptionArn}
+  }
+
+  def updateSubscription(subscription: Subscription) = {
+    val updatedSubs = subscription :: subscriptions(subscription.topicArn).filter(_.arn != subscription.arn)
+    subscriptions += (subscription.topicArn -> updatedSubs)
+
+    val producer = if(subscription.isRawMessageDelivery) {
+      context.system.actorOf(RawProducerActor.props(subscription.endpoint, subscription.arn, subscription.topicArn))
+    } else {
+      context.system.actorOf(ProducerActor.props(subscription.endpoint, subscription.arn, subscription.topicArn))
+    }
+    
+    //update the producer
+    actorPool += (subscription.arn -> producer)
+  }
+
+  def setAttribute(subscriptionArn: String, attributeName: String, attributeValue: String) = {
+    log.info(s"Setting ${attributeName} to ${attributeValue} for ${subscriptionArn}")
+    findSubscription(subscriptionArn).map { sub =>
+      val updated = sub.copy(subscriptionAttributes = sub.subscriptionAttributes + (attributeName -> attributeValue))
+      updateSubscription(updated)
+      Success
+    } getOrElse {
+      log.error(s"No subscription found for ${subscriptionArn}")
+       Failure(new Exception("Not Found"))
+    }
+  }
+  def getAttributes(subscriptionArn: String): Option[Map[String,String]] = {
+    findSubscription(subscriptionArn).map { sub =>
+      sub.subscriptionAttributes ++ Map(
+        "SubscriptionArn" -> sub.arn,
+        "TopicArn" -> sub.topicArn,
+        "Owner" -> sub.owner
+      )
     }
   }
 
@@ -73,7 +118,7 @@ class SubscribeActor(dbActor: ActorRef) extends Actor with ActorLogging {
     val producer = context.system.actorOf(ProducerActor.props(subscription.endpoint, subscription.arn, subscription.topicArn))
     val listByTopic = subscription :: subscriptions.getOrElse(subscription.topicArn, List())
 
-    actorPool += (subscription -> producer)
+    actorPool += (subscription.arn -> producer)
     subscriptions += (subscription.topicArn -> listByTopic)
   }
 
@@ -142,6 +187,8 @@ class SubscribeActor(dbActor: ActorRef) extends Actor with ActorLogging {
     case CmdListSubscriptionsByTopic(topicArn) => sender ! listSubscriptionsByTopic(topicArn)
     case CmdListSubscriptions() => sender ! listSubscriptions()
     case CmdFanOut(topicArn, message) => sender ! fanOut(topicArn, message)
+    case CmdSetSubscriptionAttributes(subscriptionArn, attributeName, attributeValue) => sender ! setAttribute(subscriptionArn, attributeName, attributeValue)
+    case CmdGetSubscriptionAttributes(subscriptionArn) => sender ! getAttributes(subscriptionArn)
     case configuration: Configuration => load(configuration)
   }
 }
--- src/main/scala/me/snov/sns/model/Subscription.scala
+++ src/main/scala/me/snov/sns/model/Subscription.scala
@@ -7,9 +7,18 @@ case class Subscription(
                          owner: String,
                          topicArn: String,
                          protocol: String,
-                         endpoint: String
-                       )
+                         endpoint: String,
+                         subscriptionAttributes: Map[String, String] = Map.empty
+                       ) {
+
+  def isRawMessageDelivery: Boolean = {
+    subscriptionAttributes
+      .get("RawMessageDelivery")
+      .map(java.lang.Boolean.parseBoolean(_))
+      .getOrElse(false)
+  }
+}
 
 object Subscription extends DefaultJsonProtocol {
-  implicit val format = jsonFormat5(Subscription.apply)
+  implicit val format = jsonFormat6(Subscription.apply)
 }
